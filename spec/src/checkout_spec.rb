e269f4340e89ed0174ecab612b767066a41f982e
Henrique Breim <henrique@breim.com.br>
2025-09-01 01:57:30 +0200

    test: create checkout

---
+++ spec/src/checkout_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require './src/checkout'
+
+RSpec.describe Checkout do
+  let(:cart) { Cart.new }
+  let(:checkout) { described_class.new(cart) }
+  let(:book) { Product.new(name: 'Harry Potter', price: 12.49, category: 'book', imported: false) }
+  let(:imported_book) { Product.new(name: 'Imported Book', price: 12.49, category: 'book', imported: true) }
+
+  describe '#initialize' do
+    it 'initializes with a cart and a text parser' do
+      expect(checkout.cart).to be_a(Cart)
+    end
+  end
+
+  describe '#total_with_taxes' do
+    it 'when the product is exempt with taxes' do
+      cart.add_product(book)
+      expect(checkout.total_with_taxes).to eql(12.49)
+    end
+
+    it 'when the product have taxes' do
+      cart.add_product(imported_book)
+      expect(checkout.total_with_taxes).to eql(13.14)
+    end
+  end
+
+  describe '#total_taxes_value' do
+    it 'when the product is exempt with taxes' do
+      cart.add_product(book)
+      expect(checkout.total_taxes_value).to eql(0.0)
+    end
+
+    it 'when the product have taxes' do
+      cart.add_product(imported_book)
+      expect(checkout.total_taxes_value).to eql(0.65)
+    end
+  end
+end
