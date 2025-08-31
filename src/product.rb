b266323badee33c9bf7c437fb0b0b85347f38f58
Henrique Breim <henrique@breim.com.br>
2025-09-01 01:42:53 +0200

    fix: remove params from initializer and add price_with_taxes

--- src/product.rb
+++ src/product.rb
@@ -2,13 +2,14 @@
 
 class Product
   attr_reader :name, :price, :category, :imported
-  attr_accessor :tax_amount
+  attr_accessor :tax_amount, :price_with_taxes
 
-  def initialize(name:, price:, category:, imported:, tax_amount: 0)
+  def initialize(name:, price:, category:, imported:)
     @name = name
     @price = price
     @category = category
     @imported = imported
-    @tax_amount = tax_amount
+    @tax_amount = 0
+    @price_with_taxes = 0
   end
 end
