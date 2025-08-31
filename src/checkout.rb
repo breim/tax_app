758fbc8d2087bf0bf13700450523c51d76d9a1b7
Henrique Breim <henrique@breim.com.br>
2025-09-01 01:57:42 +0200

    feat: create checkout

---
+++ src/checkout.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require './src/cart'
+
+class Checkout
+  attr_reader :cart
+
+  def initialize(cart)
+    @cart = cart
+  end
+
+  def total_with_taxes
+    @cart.products.sum { |product| product&.price_with_taxes }
+  end
+
+  def total_taxes_value
+    @cart.products.sum { |product| product&.tax_amount }
+  end
+end
